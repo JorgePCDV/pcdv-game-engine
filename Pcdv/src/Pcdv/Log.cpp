a136e07829d0042805d1fa831aaa934f989843b5
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2023-06-24 23:00:15 +0100

    Log::init

--- Pcdv/src/Pcdv/Log.cpp
+++ Pcdv/src/Pcdv/Log.cpp
@@ -7,5 +7,11 @@ namespace Pcdv
 
 	void Log::Init()
 	{
+		spdlog::set_pattern("%^[%T] %n: %v%$");
+		s_CoreLogger = spdlog::stdout_color_mt("PCDV");
+		s_CoreLogger->set_level(spdlog::level::trace);
+
+		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_ClientLogger->set_level(spdlog::level::trace);
 	}
 }
